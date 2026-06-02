682cd3071d5ca7d301c893feb3c959662dea8eac
dukeoghenefejiroduke <dukeoghenefejiroduke@gmail.com>
2026-06-02 01:39:04 +0100

    fix(mobile): explicitly disable package exports in metro config

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,7 +3,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add support for solving the "exports" issue if necessary
-// config.resolver.unstable_enablePackageExports = true;
+// Disable package exports as it causes issues with some libraries in this environment
+config.resolver.unstable_enablePackageExports = false;
+
+// Ensure common extensions are prioritized correctly
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
 
 module.exports = config;
