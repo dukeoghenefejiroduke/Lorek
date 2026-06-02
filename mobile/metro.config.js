69efae4b7980edbc67117b41c02ca481af481a34
dukeoghenefejiroduke <dukeoghenefejiroduke@gmail.com>
2026-06-02 01:13:59 +0100

    fix(mobile): clean install and add metro config to resolve exports

---
+++ mobile/metro.config.js
@@ -0,0 +1,9 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+// Add support for solving the "exports" issue if necessary
+config.resolver.unstable_enablePackageExports = true;
+
+module.exports = config;
