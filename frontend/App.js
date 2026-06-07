31fe7ab4acb0852ffdca10cb1612d14d7889a5e6
dukeoghenefejiroduke <dukeoghenefejiroduke@gmail.com>
2026-06-07 03:45:16 +0100

    Add Codemagic config

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import * as Sentry from '@sentry/react-native';
 
-export default function App() {
+Sentry.init({
+  dsn: 'https://42bed7edc2db8f533c938a38e74c5fd9@o4511521656078336.ingest.de.sentry.io/4511521660731472',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Minimal App - If you see this, the basic APK build is working.</Text>
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
